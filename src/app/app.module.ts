346e0f56d4141b14627ee19c2d7cb92db3c2e591
alvarezld <luisd.alvarez160880@gmail.com>
2023-08-14 04:54:24 -0300

    TP Final ANGULAR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ServicesComponent } from './services/services.component';
+import { IntroComponent } from './intro/intro.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { WhyUsComponent } from './why-us/why-us.component';
+import { PortFolioComponent } from './port-folio/port-folio.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { EquipoComponent } from './equipo/equipo.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ServicesComponent,
+    IntroComponent,
+    AboutUsComponent,
+    WhyUsComponent,
+    PortFolioComponent,
+    ClientesComponent,
+    EquipoComponent,
+    ContactoComponent,
+    TestimonialsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/port-folio/port-folio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-port-folio',
+  templateUrl: './port-folio.component.html',
+  styleUrls: ['./port-folio.component.css']
+})
+export class PortFolioComponent {
+
+}
